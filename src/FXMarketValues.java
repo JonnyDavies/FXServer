8c8c449e52bb32186687598b30885ea72a28a03e
Jonathan Davies <jonathandavies27@gmail.com>
2017-02-01 12:56:00 +0000

    Using BigDecimals array

--- src/FXMarketValues.java
+++ src/FXMarketValues.java
@@ -9,7 +9,7 @@ public class FXMarketValues {
   {
     start = "Done";
     
-    num[0] = new BigDecimal("1.00001");
+    num[0] = new BigDecimal("1.001");
     num[1] = new BigDecimal("1.00002");
     num[2] = new BigDecimal("1.00003");
     num[3] = new BigDecimal("2.00001");
@@ -17,7 +17,7 @@ public class FXMarketValues {
   
   public void incrementValues()
   {
-    num[0] = num[0].subtract(new BigDecimal ("0.00001"));
+    num[0] = num[0].add(new BigDecimal ("0.001"));
     num[1] = num[1].add(new BigDecimal ("0.00004"));
     num[2] = num[2].add(new BigDecimal ("0.00002"));
     num[3] = num[3].add(new BigDecimal ("0.00008"));
